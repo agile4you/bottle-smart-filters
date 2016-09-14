bec8d93ea44a8e3cbf085cbc127693b30dd394de
PyB1l <vpapavasil@gmail.com>
2016-09-14 15:38:14 +0300

    added requirements.txt

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='bottle-filters',
+    version='0.1',
+    packages=['bottle_filters'],
+    url='https://github.com/agile4you/bottle-filters',
+    license='GLPv3',
+    author='pav',
+    author_email='vpapavasil@gmail.com',
+    description='Bottle.py QueryString Params consistent handling.',
+    extras_require={
+        'test': ['pytest', 'bottle', 'six'],
+    }
+)
