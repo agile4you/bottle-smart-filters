fbefd0568eaee6c33df63dfc70da65de48501a8f
PyB1l <vpapavasil@gmail.com>
2017-05-08 19:31:22 +0300

    Added python version support

--- setup.py
+++ setup.py
@@ -1,15 +1,26 @@
 from distutils.core import setup
+import re
+import ast
+
+
+_version_re = re.compile(r'__version__\s+=\s+(.*)')
+
+with open('bottle_smart_filters/__init__.py', 'rb') as f:
+    version = str(ast.literal_eval(_version_re.search(
+        f.read().decode('utf-8')
+    ).group(1)))
+
 
 setup(
     name='bottle-smart-filters',
-    version='0.1',
+    version=version,
     packages=['bottle_smart_filters'],
     url='https://github.com/agile4you/bottle-smart-filters',
     license='GLPv3',
     author='pav',
     author_email='vpapavasil@gmail.com',
     description='Bottle.py QueryString Params smart guessing.',
-    extras_require={
-        'test': ['pytest', 'bottle', 'ujson', 'webtest'],
-    }
+    install_requires=[
+        'pytest', 'bottle', 'ujson', 'webtest'
+    ]
 )
